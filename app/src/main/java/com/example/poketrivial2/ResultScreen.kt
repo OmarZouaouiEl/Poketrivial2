bad9bd37ae167562f489cb950782b3ef1c51855f
Omar <omar.zouaoui.7e7@itb.cat>
2024-01-31 10:11:56 +0100

    rwplpwrvwr

--- app/src/main/java/com/example/poketrivial2/ResultScreen.kt
+++ app/src/main/java/com/example/poketrivial2/ResultScreen.kt
@@ -3,12 +3,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
