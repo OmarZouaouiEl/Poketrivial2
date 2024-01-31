beaa24bb09e7dff93eb0d15787d0e9a84c59ab5c
Omar <omar.zouaoui.7e7@itb.cat>
2024-01-31 10:06:22 +0100

    rwplpwrvwr

---
+++ app/src/main/java/com/example/poketrivial2/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.poketrivial2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import com.example.poketrivial2.ui.theme.Poketrivial2Theme
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Poketrivial2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navigationController = rememberNavController()
+                    NavHost(
+                        navController = navigationController,
+                        startDestination = Routes.SplashScreen.route
+                    ) {
+                        composable(Routes.SplashScreen.route) { Routes.SplashScreen.route }
+                        composable(Routes.MenuScreen.route) { Routes.MenuScreen.route }
+                        composable(Routes.GameScreen.route) { Routes.GameScreen.route }
+                        composable(Routes.ResultScreen.route) { Routes.ResultScreen.route }
+                        composable(Routes.SettingsScreen.route) { Routes.SettingsScreen.route}
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
